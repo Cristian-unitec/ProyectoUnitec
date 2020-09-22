b3cf2f3b529ae207180164bdd7f36d4b349bb54a
Cristian-unitec <71358588+Cristian-unitec@users.noreply.github.com>
2020-09-22 18:43:12 -0500

    Update MainActivity.kt

--- app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
+++ app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //El punto de partida de toda APP es la MainActivity (Presenter)
-//Prueba de github
+//Prueba de github desde repositorio
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
